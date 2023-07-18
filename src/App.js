97864046e49db181a7379ffe1169a0c2f5448af3
softmoca <acyc0909@gmail.com>
2023-07-18 22:31:24 +0900

    practice_reacte_router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
+import Home from "./pages/Home";
+import New from "./pages/New";
+import Diary from "./pages/Diary";
+import Edit from "./pages/Edit";
+import RouteTest from "./compoments/RouteTest";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <BrowserRouter>
+      <div className="App">
+        <h2>안녕리액트</h2>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/new" element={<New />} />
+          <Route path="/edit" element={<Edit />} />
+          <Route path="/diary" element={<Diary />} />
+        </Routes>
+        <RouteTest />
+      </div>
+    </BrowserRouter>
+  );
 }
 
 export default App;
---
+++ src/pages/Edit.js
@@ -0,0 +1,11 @@
+const Edit = () => {
+  return (
+    <div>
+      {" "}
+      <h1>Edit</h1>
+      <p>이곳은 일기 수정 입니다</p>
+    </div>
+  );
+};
+
+export default Edit;
