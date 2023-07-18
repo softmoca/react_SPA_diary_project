249a9861cdcb37f14444cd9af2f46e62ab452ca1
softmoca <acyc0909@gmail.com>
2023-07-18 23:44:07 +0900

    feat: add font layout image_asset common compoment

--- src/App.js
+++ src/App.js
@@ -5,12 +5,36 @@ import New from "./pages/New";
 import Diary from "./pages/Diary";
 import Edit from "./pages/Edit";
 import RouteTest from "./compoments/RouteTest";
+import MyButton from "./compoments/MyButton";
+import MyHeader from "./compoments/MyHeader";
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
+        <MyHeader headText={"App"} />
         <h2>안녕리액트</h2>
+        <MyButton
+          text={"버튼이올시다"}
+          onClick={() => alert("버튼클릭")}
+          type={"positive"}
+        />
+        <MyButton
+          text={"버튼이올시다"}
+          onClick={() => alert("버튼클릭")}
+          type={"negative"}
+        />
+        <MyButton
+          text={"버튼이올시다"}
+          onClick={() => alert("버튼클릭")}
+          type={"positive"}
+        />
+        {/* <img src={process.env.PUBLIC_URL + `/assets/emotion1.png`}></img>
+        <img src={process.env.PUBLIC_URL + `/assets/emotion2.png`}></img>
+        <img src={process.env.PUBLIC_URL + `/assets/emotion3.png`}></img>
+        <img src={process.env.PUBLIC_URL + `/assets/emotion4.png`}></img>
+        <img src={process.env.PUBLIC_URL + `/assets/emotion5.png`}></img> */}
+
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/new" element={<New />} />
