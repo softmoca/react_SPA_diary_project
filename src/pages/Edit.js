26fefeccd6ddeba0d85f525ece101b271b54e463
softmoca <acyc0909@gmail.com>
2023-07-18 22:55:32 +0900

    practice_router_Next_level

--- src/pages/Edit.js
+++ src/pages/Edit.js
@@ -1,9 +1,36 @@
+import { useNavigate, useSearchParams } from "react-router-dom";
+
 const Edit = () => {
+  const navigat = useNavigate();
+
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const id = searchParams.get("id");
+  console.log("id: ", id);
+
+  const mode = searchParams.get("mode");
+  console.log("mode: ", mode);
+
   return (
     <div>
       {" "}
       <h1>Edit</h1>
       <p>이곳은 일기 수정 입니다</p>
+      <button onClick={() => setSearchParams({ who: "moca" })}> QS </button>
+      <button
+        onClick={() => {
+          navigat("/home");
+        }}
+      >
+        HOME으로 가기
+      </button>
+      <button
+        onClick={() => {
+          navigat(-1);
+        }}
+      >
+        뒤로 가기
+      </button>
     </div>
   );
 };
